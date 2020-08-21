f49c3cb0a403071cef8b97bcc9d451f5fb73e5b2
xuantruongdang <xuantruongdang.it@gmail.com>
2020-08-21 22:38:11 +0700

    config MOI

--- utils/MOI.py
+++ utils/MOI.py
@@ -3,6 +3,7 @@ import numpy as np
 import imutils
 import argparse
 import os 
+import random 
 
 class Point: 
     def __init__(self, x, y): 
@@ -12,30 +13,50 @@ class Point:
 def config_cam(img, cfg):
     arr = cfg.CAM.ROI_DEFAULT
     arr1 = cfg.CAM.TRACKING_ROI
-
+    num_of_MOI = cfg.CAM.NUMBER_MOI
+    
+    list_moi = []
+    line = cfg.CAM.LINE
+    line_startX = []
+    line_endX = []
+    line_startY = []
+    line_endY = []
     pts = np.array(arr)
     pts1 = np.array(arr1)
 
     # plot ROI
     cv2.drawContours(img, [pts], -1, (0, 0, 255), 2)
     cv2.drawContours(img, [pts1], -1, (0, 255, 0), 2)
+    
+    #for i in range (num_of_MOI):
+    #  name_of_line = 'LINE'+ str(i)
+    #  line.append (cfg.CAM.name_of_line)
+
+    for i in line:
+      line_startX.append (i[0][0])
+      line_startY.append (i[0][1])
+      line_endX.append (i[1][0])
+      line_endY.append (i[1][1])
+      
+    for i in range (len(line_startX)):
+      cv2.line(img, (line_startX[i], line_startY[i]), (line_endX[i], line_endY[i]),(random.randint(0,255), random.randint(0,255), 0))
 
     # plot calibrations lines
-    line1 = cfg.CAM.LINE1
-    line2 = cfg.CAM.LINE2
+    # line1 = cfg.CAM.LINE1
+    # line2 = cfg.CAM.LINE2
 
-    line1_startX = line1[0][0]
-    line1_startY = line1[0][1]
-    line1_endX = line1[1][0]
-    line1_endY = line1[1][1]
+    # line1_startX = line1[0][0]
+    # line1_startY = line1[0][1]
+    # line1_endX = line1[1][0]
+    # line1_endY = line1[1][1]
 
-    line2_startX = line2[0][0]
-    line2_startY = line2[0][1]
-    line2_endX = line2[1][0]
-    line2_endY = line2[1][1]
+    # line2_startX = line2[0][0]
+    # line2_startY = line2[0][1]
+    # line2_endX = line2[1][0]
+    # line2_endY = line2[1][1]
 
-    cv2.line(img, (line1_startX, line1_startY), (line1_endX, line1_endY),(0, 255, 0))
-    cv2.line(img, (line2_startX, line2_startY), (line2_endX, line2_endY),(150, 0, 0))
+    # cv2.line(img, (line1_startX, line1_startY), (line1_endX, line1_endY),(0, 255, 0))
+    # cv2.line(img, (line2_startX, line2_startY), (line2_endX, line2_endY),(150, 0, 0))
 
     return img
 
@@ -91,31 +112,48 @@ def transform(point_1, point_2):
     return point_1, point_2
 
 def compute_MOI(cfg, point_in, point_out):
-    line1 = cfg.CAM.LINE1
-    line2 = cfg.CAM.LINE2
+    num_of_MOI = cfg.CAM.NUMBER_MOI
+    list_moi = []
+    line = cfg.CAM.LINE
+    line_leftpoint = []
+    line_rightpoint = []
 
-    line1_leftpoint = line1[0]
-    line1_rightpoint = line1[1]
-    line2_leftpoint = line2[0]
-    line2_rightpoint = line2[1]
+    for i in line:
+      line_leftpoint.append (i[0])
+      line_rightpoint.append (i[1])
+
+
+    # line1_leftpoint = line1[0]
+    # line1_rightpoint = line1[1]
+    # line2_leftpoint = line2[0]
+    # line2_rightpoint = line2[1]
 
     # transform to Point format
     p2, q2 = transform(point_in, point_out)
-    line1_leftpoint, line1_rightpoint = transform(line1_leftpoint, line1_rightpoint)
-    line2_leftpoint, line2_rightpoint = transform(line2_leftpoint, line2_rightpoint)
+    for i in range(len(line_leftpoint)):
+      line_leftpoint[i], line_rightpoint[i] = transform (line_leftpoint[i], line_rightpoint[i])
+
+
+    # line1_leftpoint, line1_rightpoint = transform(line1_leftpoint, line1_rightpoint)
+    # line2_leftpoint, line2_rightpoint = transform(line2_leftpoint, line2_rightpoint)
 
     moi = -1
     orient = None
 
     # Vector V has first point p2 and last point q2: present the movement of vehicle
     # if V intersect line1, MOI = 1
-    if doIntersect(line1_leftpoint, line1_rightpoint, p2, q2):
-        moi = 1
-        orient = orientation(line1_leftpoint, line1_rightpoint, p2)
+    for i in range (len (line_leftpoint)):
+      if (doIntersect(line_leftpoint[i], line_rightpoint[i], p2, q2)):
+        moi = i + 1
+        orient = orientation (line_leftpoint[i], line_rightpoint[i], p2)
 
-    # if V intersect line2, MOI = 2
-    if doIntersect(line2_leftpoint, line2_rightpoint, p2, q2):
-        moi = 2
-        orient = orientation(line2_leftpoint, line2_rightpoint, p2)
+    # if doIntersect(line1_leftpoint, line1_rightpoint, p2, q2):
+    #     moi = 1
+    #     orient = orientation(line1_leftpoint, line1_rightpoint, p2)
+
+    # # if V intersect line2, MOI = 2
+    # if doIntersect(line2_leftpoint, line2_rightpoint, p2, q2):
+    #     moi = 2
+    #     orient = orientation(line2_leftpoint, line2_rightpoint, p2)
 
     return moi, orient
